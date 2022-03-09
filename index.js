c047aad6d0225d1b00a1fa7be587efc4b939d9f9
= <=>
2022-03-09 13:07:11 +0530

    latest

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+*primary file for the API
+*
+*/
+
+// Dependencies
+const server = require('./lib/server');
+//const workers = require('./lib/workers');
+
+// Declare the app
+const app = {};
+
+//Init function
+app.init = function(){
+  //start the Server
+  server.init();
+
+  // start the workers
+//  workers.init();
+};
+
+// Execute
+app.init();
+
+
+// Export the app
+module.exports = app;
